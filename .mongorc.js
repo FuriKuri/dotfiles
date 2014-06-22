370e901695475f0526e026a4b7b6e66f2b769182
Theo Pack <tf.pack@gmail.com>
2014-06-22 22:29:14 +0200

    modify mongorc

--- .mongorc.js
+++ .mongorc.js
@@ -1,1 +1,9 @@
-var prompt = function() { return db+" @ 🐙  > "; }
+var host = db.serverStatus().host;
+if (host === "coin-a-c-190.localdomain") {
+	host = "🐳"; 
+} else if (host === "naga.localdomain") {
+	host = "🐠";
+} else {
+	host = "🐬";
+}
+var prompt = function() { return db+" at " + host + "  > "; }
