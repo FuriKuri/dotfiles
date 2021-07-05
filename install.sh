64ec73dc68874a1fc94c4207e591330081b5b3f6
Theo Pack <theo.pack@innoq.com>
2021-07-05 10:12:02 +0200

    Add missing symlinks

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ cd $HOME
 
 mv $HOME/.zshrc .zshrc_backup
 ln -s $HOME/.dotfiles/zshrc $HOME/.zshrc
+ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
+ln -s $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
